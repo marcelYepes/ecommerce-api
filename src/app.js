74c613982afd63550299ae940eccdab59f8debcf
marcelYepes <marcelyepesqf@gmail.com>
2023-07-03 11:51:44 -0500

    purchase test

--- src/app.js
+++ src/app.js
@@ -1,26 +1,31 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
-const router = require('./routes');
-const errorHandler = require('./utils/errorHandler');
-require('dotenv').config();
+const express = require("express")
+const helmet = require("helmet")
+const cors = require("cors")
+const router = require("./routes")
+const errorHandler = require("./utils/errorHandler")
+const path = require("path")
+require("dotenv").config()
 
 // Esta es nuestra aplicación
-const app = express();
+const app = express()
 
-// Middlewares 
-app.use(express.json());
-app.use(helmet({
+// Middlewares
+app.use(express.json())
+app.use(
+  helmet({
     crossOriginResourcePolicy: false,
-}));
-app.use(cors());
+  })
+)
+app.use(cors())
 
-app.use('/api/v1', router);
-app.get('/', (req, res) => {
-    return res.send("Welcome to express!");
+app.use(express.static(path.join(__dirname, "public")))
+
+app.use("/api/v1", router)
+app.get("/", (req, res) => {
+  return res.send("Welcome to express!")
 })
 
 // middlewares después de las rutas
 app.use(errorHandler)
 
-module.exports = app;
+module.exports = app
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,14 @@ const routerCategory = require("./category.router")
 const routerProduct = require("./product.router")
 const routerCart = require("./cart.router")
 const routerPurchase = require("./purchase.router")
+const routerProductImg = require("./productImg.router")
 const router = express.Router()
 
 router.use("/users", routerUser)
 router.use("/categories", routerCategory)
 router.use("/products", routerProduct)
 router.use("/cart", routerCart)
-router.use("/purchases", routerPurchase)
+router.use("/purchase", routerPurchase)
+router.use("/product_images", routerProductImg)
 
 module.exports = router
